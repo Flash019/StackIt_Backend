d1ebeb2f25a5d517972fa8a9f7f17ae4d3c6cd17
Soumyabrata Mahapatra <soumyabratamahapatra019@gmail.com>
2025-08-28 19:12:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,7 @@ app = FastAPI(title="StackIt", debug=True)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
+       "*"
     ],  # <-- specify allowed origins explicitly, no "*"
     allow_credentials=True,
     allow_methods=["*"],
